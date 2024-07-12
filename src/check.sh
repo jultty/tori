75ff3a30091b17fc6499503e859fb464343246e9
jutty <j@jutty.dev>
2024-07-12 07:24:00 -0300

    Standardize indentation

--- src/check.sh
+++ src/check.sh
@@ -1,9 +1,9 @@
 check() {
-	base_files="$(scan_directory "$CONFIG_ROOT/base")"
-	bkp_files="$(scan_directory "$CONFIG_ROOT/bkp")"
+  base_files="$(scan_directory "$CONFIG_ROOT/base")"
+  bkp_files="$(scan_directory "$CONFIG_ROOT/bkp")"
 
-	log debug "collected base files:\n$base_files"
-	log debug "collected bkp files:\n$bkp_files"
+  log debug "collected base files:\n$base_files"
+  log debug "collected bkp files:\n$bkp_files"
 
-	scan_packages
+  scan_packages
 }
