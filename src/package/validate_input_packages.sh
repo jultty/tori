7769565a2160951039aa2d4797dad5f4737ffc70
jutty <j@jutty.dev>
2024-07-12 08:28:21 -0300

    Update cache only when needed

--- src/package/validate_input_packages.sh
+++ src/package/validate_input_packages.sh
@@ -8,15 +8,18 @@ validate_input_packages() {
 
   echo "$package_list" | xargs | sed 's/ /\n/g' | while read -r package; do
 
-  if echo "$package" | grep -q "$invalid_characters_pattern"; then
-    log user "Invalid package: $package (contains invalid characters)"
-    return 1
-  fi
-  if grep -qw "$package" "$PACKAGE_CACHE"; then
-    log debug "Found in package cache: $package"
-  else
-    log user "Invalid package: $package (not found in package cache)"
-    return 1
-  fi
-done
+    if echo "$package" | grep -q "$invalid_characters_pattern"; then
+      log user "Invalid package: $package (contains invalid characters)"
+      return 1
+    fi
+
+    update_package_cache
+
+    if grep -qw "$package" "$PACKAGE_CACHE"; then
+      log debug "Found in package cache: $package"
+    else
+      log user "Invalid package: $package (not found in package cache)"
+      return 1
+    fi
+  done
 }
