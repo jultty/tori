7868149bbfd7ab7027b5cc4bcb44ec35d677111b
jutty <j@jutty.dev>
2024-07-20 16:32:36 -0300

    Allow multiple strategies on editor resolution

--- src/package/package_conflict_input_parser.sh
+++ src/package/package_conflict_input_parser.sh
@@ -17,7 +17,8 @@ package_conflict_input_parser() {
 
   $EDITOR "$input"
 
-  choices="$(cat "$input" | grep -v '^#' | grep '.')" > "$input_choices"
+  choices="$(cat "$input" | grep -v '^#' | grep '.')"
+  echo "$choices" > "$input_choices"
 
   # validation
   while read -r action package; do
@@ -35,25 +36,27 @@ package_conflict_input_parser() {
     elif [ "$action" = skip ] || [ "$action" = s ]; then
       log debug "[package_conflict_input_parser] Skipped: $package"
     else
-      log user "Invalid action provided: $action"
+      log user "Invalid action provided for $package: $action"
     fi
   done < "$input_choices"
 
   # actual system or configuration change
-  echo "$choices" | while read -r action package; do
-    if [ "$action" = install ] || [ "$action" = i ]; then
-      package_manager install "$packages_to_install"
-    elif [ "$action" = uninstall ] || [ "$action" = u ]; then
-      debug info "Calling package manager to uninstall $packages_to_uninstall"
-      package_manager uninstall "$packages_to_uninstall"
-    elif [ "$action" = add ] || [ "$action" = a ]; then
-      track_packages "$package"
-    elif [ "$action" = remove ] || [ "$action" = r ]; then
-      untrack_packages "$package"
-    fi
-  done
-}
+  if [ -n "$packages_to_install" ]; then
+    package_manager install "$packages_to_install"
+  fi
 
+  if [ -n "$packages_to_uninstall" ]; then
+    package_manager uninstall "$packages_to_uninstall"
+  fi
+
+  if [ -n "$packages_to_track" ]; then
+    track_packages "$packages_to_track"
+  fi
+
+  if [ -n "$packages_to_untrack" ]; then
+    untrack_packages "$packages_to_untrack"
+  fi
+}
 
 help_text_generator() {
   local conflict_type="$1"
