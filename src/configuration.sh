2db794739d7b1c8e7846d3b40a9f670d7fc83ed6
jutty <j@jutty.dev>
2025-01-06 04:02:34 -0300

    Further decouple configuration processing functions

--- src/configuration.sh
+++ src/configuration.sh
@@ -1,5 +1,9 @@
 # configuration processing functions
+# these functions serve as an interface between configuration files and inner logic
 
+# takes a directory as its single argument
+# returns the relative paths for each file contained within, separated by newlines
+# only regular files are returned, symbolic links are not followed
 scan_directory() {
   local target="$1"
   local files=
@@ -18,24 +22,24 @@ scan_directory() {
   printf "%b" "$files"
 }
 
+# takes a path to a package list file
+# returns 0 if packages match, 1 if they differ
 scan_packages() {
+    package_file="$1"
 
-  if ! [ -f "$CONFIG_ROOT/packages" ]; then
-    log debug "No packages file found in $CONFIG_ROOT"
+  if ! [ -f  "$package_file" ]; then
+    log debug "[scan_packages] No file found at $package_file"
     return 0
   fi
 
   system_packages=$(package_manager get_manually_installed)
-  user_packages=$(get_user_packages)
+  user_packages=$(cat "$package_file" | sort | uniq)
 
   if [ "$system_packages" = "$user_packages" ]; then
-    log debug "Packages match"
+    log debug "[scan_packages] Packages match"
+    return 0
   else
     log user "System and configuration packages differ"
-    resolve_packages
+    return 1
   fi
-}
-
-get_user_packages() {
-  cat "$CONFIG_ROOT/packages" | sort | uniq
 }
