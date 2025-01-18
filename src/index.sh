4df6c40ddded341b362d9106cf62c4f6b4602429
jutty <j@jutty.dev>
2025-01-18 03:59:22 -0300

    Update src index

--- src/index.sh
+++ src/index.sh
@@ -1,20 +1,20 @@
-. "$TORI_ROOT/src/setup/directories.sh"
+. "$TORI_ROOT/src/setup/directory.sh"
 . "$TORI_ROOT/src/setup/shell.sh"
 
-. "$TORI_ROOT/src/parsers/os_parser.sh"
-. "$TORI_ROOT/src/parsers/option_parser.sh"
-. "$TORI_ROOT/src/parsers/configuration_parser.sh"
+. "$TORI_ROOT/src/parsers/os.sh"
+. "$TORI_ROOT/src/parsers/option.sh"
+. "$TORI_ROOT/src/parsers/configuration.sh"
 
 . "$TORI_ROOT/src/ui/dialog.sh"
 
 . "$TORI_ROOT/src/commands/check.sh"
 
-. "$TORI_ROOT/src/package/package_management.sh"
-. "$TORI_ROOT/src/package/package_conflict_resolution.sh"
-. "$TORI_ROOT/src/package/package_tracking.sh"
-. "$TORI_ROOT/src/package/package_input_validator.sh"
-. "$TORI_ROOT/src/package/package_conflict_input_parser.sh"
-. "$TORI_ROOT/src/package/package_cache.sh"
+. "$TORI_ROOT/src/package/management.sh"
+. "$TORI_ROOT/src/package/conflict_resolution.sh"
+. "$TORI_ROOT/src/package/tracking.sh"
+. "$TORI_ROOT/src/package/input_validator.sh"
+. "$TORI_ROOT/src/package/conflict_input_parser.sh"
+. "$TORI_ROOT/src/package/cache.sh"
 
-. "$TORI_ROOT/src/file/file_merge.sh"
-. "$TORI_ROOT/src/file/file_backup.sh"
+. "$TORI_ROOT/src/file/merger.sh"
+. "$TORI_ROOT/src/file/backup.sh"
